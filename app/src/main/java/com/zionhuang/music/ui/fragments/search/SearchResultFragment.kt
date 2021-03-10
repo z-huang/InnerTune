315540d13fc0d520a407ac758059e2581fff2208
z-huang <zionhuang1107@gmail.com>
2021-03-10 15:47:41 +0800

    [Fix] Import

--- app/src/main/java/com/zionhuang/music/ui/fragments/search/SearchResultFragment.kt
+++ app/src/main/java/com/zionhuang/music/ui/fragments/search/SearchResultFragment.kt
@@ -6,6 +6,7 @@ import android.view.MenuInflater
 import android.view.MenuItem
 import android.view.View
 import androidx.core.os.bundleOf
+import androidx.core.view.isVisible
 import androidx.fragment.app.activityViewModels
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.lifecycleScope
