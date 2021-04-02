554172133dc31827207d4b03f3922b5930806852
z-huang <zionhuang1107@gmail.com>
2021-04-02 12:40:30 +0800

    [Fix] Lint

--- app/src/main/java/com/zionhuang/music/ui/fragments/YouTubePlaylistFragment.kt
+++ app/src/main/java/com/zionhuang/music/ui/fragments/YouTubePlaylistFragment.kt
@@ -3,7 +3,9 @@ package com.zionhuang.music.ui.fragments
 import android.graphics.Color
 import android.os.Bundle
 import android.view.View
+import androidx.core.os.bundleOf
 import androidx.core.view.isVisible
+import androidx.fragment.app.activityViewModels
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.lifecycleScope
 import androidx.navigation.fragment.navArgs
@@ -11,21 +13,25 @@ import androidx.paging.LoadState
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.google.android.material.transition.MaterialContainerTransform
 import com.zionhuang.music.R
-import com.zionhuang.music.databinding.FragmentSearchResultBinding
+import com.zionhuang.music.databinding.LayoutRecyclerviewBinding
 import com.zionhuang.music.extensions.addOnClickListener
 import com.zionhuang.music.extensions.themeColor
 import com.zionhuang.music.ui.adapters.InfoItemAdapter
 import com.zionhuang.music.ui.adapters.LoadStateAdapter
 import com.zionhuang.music.ui.fragments.base.BindingFragment
+import com.zionhuang.music.viewmodels.PlaybackViewModel
 import com.zionhuang.music.viewmodels.YouTubePlaylistViewModel
+import com.zionhuang.music.youtube.extractors.YouTubeStreamExtractor
 import kotlinx.coroutines.flow.collectLatest
 import kotlinx.coroutines.launch
+import org.schabi.newpipe.extractor.stream.StreamInfoItem
 
-class YouTubePlaylistFragment : BindingFragment<FragmentSearchResultBinding>() {
+class YouTubePlaylistFragment : BindingFragment<LayoutRecyclerviewBinding>() {
     private val args: YouTubePlaylistFragmentArgs by navArgs()
     private val url by lazy { args.url }
 
     private val viewModel by viewModels<YouTubePlaylistViewModel>()
+    private val playbackViewModel by activityViewModels<PlaybackViewModel>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -54,7 +60,12 @@ class YouTubePlaylistFragment : BindingFragment<FragmentSearchResultBinding>() {
             layoutManager = LinearLayoutManager(requireContext())
             adapter = infoItemAdapter.withLoadStateFooter(LoadStateAdapter { infoItemAdapter.retry() })
             addOnClickListener { pos, _ ->
+                val item = infoItemAdapter.getItemByPosition(pos)
+                if (item is StreamInfoItem) {
+                    playbackViewModel.playMedia(requireActivity(), YouTubeStreamExtractor.extractId(item.url)!!, bundleOf(
 
+                    ))
+                }
             }
         }
 
