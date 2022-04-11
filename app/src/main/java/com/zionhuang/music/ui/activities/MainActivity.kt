3d385f853b4bb8bf4e7f12ea3aa948d8c5f1ad08
Zion Huang <zionhuang1107@gmail.com>
2022-04-11 12:37:53 +0800

    [Feature] Deep link for YouTube urls

--- app/src/main/java/com/zionhuang/music/ui/activities/MainActivity.kt
+++ app/src/main/java/com/zionhuang/music/ui/activities/MainActivity.kt
@@ -1,12 +1,16 @@
 package com.zionhuang.music.ui.activities
 
+import android.content.Intent
+import android.content.Intent.ACTION_VIEW
+import android.net.Uri
 import android.os.Bundle
 import android.support.v4.media.session.PlaybackStateCompat.STATE_NONE
+import android.util.Log
 import android.view.ActionMode
 import android.view.View
+import androidx.core.os.bundleOf
 import androidx.core.view.isVisible
 import androidx.lifecycle.ViewModelProvider
-import androidx.lifecycle.lifecycleScope
 import androidx.lifecycle.map
 import androidx.navigation.NavController
 import androidx.navigation.NavDestination
@@ -15,17 +19,22 @@ import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupWithNavController
 import com.google.android.material.bottomsheet.BottomSheetBehavior
 import com.google.android.material.bottomsheet.BottomSheetBehavior.*
-import com.google.android.material.snackbar.Snackbar
 import com.zionhuang.music.R
+import com.zionhuang.music.constants.MediaConstants.EXTRA_QUEUE_DATA
+import com.zionhuang.music.constants.MediaConstants.QUEUE_YT_SINGLE
 import com.zionhuang.music.databinding.ActivityMainBinding
+import com.zionhuang.music.extensions.TAG
 import com.zionhuang.music.extensions.getDensity
 import com.zionhuang.music.extensions.replaceFragment
+import com.zionhuang.music.models.QueueData
 import com.zionhuang.music.ui.fragments.BottomControlsFragment
 import com.zionhuang.music.ui.widgets.BottomSheetListener
 import com.zionhuang.music.ui.widgets.MainFloatingActionButton
 import com.zionhuang.music.viewmodels.PlaybackViewModel
 import com.zionhuang.music.viewmodels.SongsViewModel
-import kotlinx.coroutines.launch
+import com.zionhuang.music.youtube.NewPipeYouTubeHelper.extractVideoId
+import com.zionhuang.music.youtube.NewPipeYouTubeHelper.getLinkType
+import org.schabi.newpipe.extractor.StreamingService.LinkType
 
 class MainActivity : BindingActivity<ActivityMainBinding>(), NavController.OnDestinationChangedListener {
     override fun getViewBinding() = ActivityMainBinding.inflate(layoutInflater)
@@ -54,6 +63,26 @@ class MainActivity : BindingActivity<ActivityMainBinding>(), NavController.OnDes
 //                }
 //                .show()
 //        }
+    }
+
+    override fun onNewIntent(intent: Intent?) {
+        super.onNewIntent(intent)
+        when (intent?.action) {
+            ACTION_VIEW -> {
+                val url = intent.data.toString()
+                when (getLinkType(url)) {
+                    LinkType.STREAM -> {
+                        val videoId = extractVideoId(url)!!
+                        playbackViewModel.playMedia(this, videoId, bundleOf(
+                            EXTRA_QUEUE_DATA to QueueData(QUEUE_YT_SINGLE, queueId = videoId)
+                        ))
+                    }
+                    LinkType.CHANNEL -> {}
+                    LinkType.PLAYLIST -> {}
+                    LinkType.NONE -> {}
+                }
+            }
+        }
     }
 
     private fun setupUI() {
@@ -89,11 +118,7 @@ class MainActivity : BindingActivity<ActivityMainBinding>(), NavController.OnDes
         }
     }
 
-    override fun onDestinationChanged(
-        controller: NavController,
-        destination: NavDestination,
-        arguments: Bundle?
-    ) {
+    override fun onDestinationChanged(controller: NavController, destination: NavDestination, arguments: Bundle?) {
         actionMode?.finish()
         if (destination.id == R.id.playlistsFragment) {
             binding.fab.show()
