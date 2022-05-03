8eea141a377cbd86b0e4d3fb11c486a7829e7197
Zion Huang <zionhuang1107@gmail.com>
2022-05-03 18:35:44 +0800

    Update makeTimeString function

--- app/src/main/java/com/zionhuang/music/utils/YouTubeUtils.kt
+++ app/src/main/java/com/zionhuang/music/utils/YouTubeUtils.kt
@@ -1,6 +1,6 @@
 package com.zionhuang.music.utils
 
-private const val MS_FORMAT = "%2\$d:%3$02d"
+private const val MS_FORMAT = "%1\$d:%2$02d"
 private const val HMS_FORMAT = "%1\$d:%2$02d:%3$02d"
 
 /**
@@ -9,14 +9,13 @@ private const val HMS_FORMAT = "%1\$d:%2$02d:%3$02d"
  * @param duration in seconds
  * @return formatted string
  */
-@Suppress("NAME_SHADOWING")
+
 fun makeTimeString(duration: Long?): String {
     if (duration == null) return "0:00"
-    var duration = duration
-    val hours = duration / 3600
-    duration %= 3600
-    val minutes = (duration / 60).toInt()
-    duration %= 60
-    val secs = duration.toInt()
-    return String.format(if (hours == 0L) MS_FORMAT else HMS_FORMAT, hours, minutes, secs)
+    var sec = duration
+    val hour = sec / 3600
+    sec %= 3600
+    val minute = (sec / 60).toInt()
+    sec %= 60
+    return if (hour == 0L) MS_FORMAT.format(minute, sec) else HMS_FORMAT.format(hour, minute, sec)
 }
