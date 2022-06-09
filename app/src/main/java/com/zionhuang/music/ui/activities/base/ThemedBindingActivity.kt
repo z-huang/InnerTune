68fef5e3f689f6b84bb0b1979a2f7bcd5dd1e8a8
Zion Huang <zionhuang1107@gmail.com>
2022-06-09 12:40:38 +0800

    [Fix] Dark mode preference type update

--- app/src/main/java/com/zionhuang/music/ui/activities/base/ThemedBindingActivity.kt
+++ app/src/main/java/com/zionhuang/music/ui/activities/base/ThemedBindingActivity.kt
@@ -2,6 +2,7 @@ package com.zionhuang.music.ui.activities.base
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatDelegate
+import androidx.appcompat.app.AppCompatDelegate.MODE_NIGHT_FOLLOW_SYSTEM
 import androidx.viewbinding.ViewBinding
 import com.google.android.material.color.DynamicColors
 import com.zionhuang.music.R
@@ -11,7 +12,16 @@ import com.zionhuang.music.utils.livedata.ThemeUtil.DEFAULT_THEME
 
 abstract class ThemedBindingActivity<T : ViewBinding> : BindingActivity<T>() {
     override fun onCreate(savedInstanceState: Bundle?) {
-        AppCompatDelegate.setDefaultNightMode(sharedPreferences.getString(getString(R.string.pref_dark_theme), AppCompatDelegate.MODE_NIGHT_FOLLOW_SYSTEM.toString())!!.toInt())
+        // Fix preference type mismatch in 0.3.0
+        try {
+            sharedPreferences.getString(getString(R.string.pref_dark_theme), "MODE_FOLLOW_SYSTEM")!!.toInt()
+        } catch (e: Exception) {
+            sharedPreferences.edit()
+                .putString(getString(R.string.pref_dark_theme), MODE_NIGHT_FOLLOW_SYSTEM.toString())
+                .commit()
+        }
+
+        AppCompatDelegate.setDefaultNightMode(sharedPreferences.getString(getString(R.string.pref_dark_theme), MODE_NIGHT_FOLLOW_SYSTEM.toString())!!.toInt())
         if (DynamicColors.isDynamicColorAvailable() && sharedPreferences.getBoolean(getString(R.string.pref_follow_system_accent), true)) {
             DynamicColors.applyToActivityIfAvailable(this)
         } else {
