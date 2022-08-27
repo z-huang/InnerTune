95e4d25a73e4d2157cb1f2eba9f690125cb1b1b9
Zion Huang <zionhuang1107@gmail.com>
2022-08-27 12:18:32 +0800

    Bump version to 0.3.3

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -17,8 +17,8 @@ android {
         applicationId = "com.zionhuang.music"
         minSdk = 26
         targetSdk = 31
-        versionCode = 9
-        versionName = "0.3.2"
+        versionCode = 10
+        versionName = "0.3.3"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
     applicationVariants.all {
