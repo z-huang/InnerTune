9a1e7a50147d735d5c0ca8a1d3a669a2116f00bd
Zion Huang <zionhuang1107@gmail.com>
2022-09-01 20:25:03 +0800

    Batch operation for YouTube song items

--- app/src/main/java/com/zionhuang/music/ui/fragments/youtube/YouTubeBrowseFragment.kt
+++ app/src/main/java/com/zionhuang/music/ui/fragments/youtube/YouTubeBrowseFragment.kt
@@ -11,6 +11,9 @@ import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.lifecycleScope
 import androidx.navigation.fragment.findNavController
 import androidx.navigation.fragment.navArgs
+import androidx.recyclerview.selection.SelectionPredicates
+import androidx.recyclerview.selection.SelectionTracker
+import androidx.recyclerview.selection.StorageStrategy
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.google.android.material.transition.MaterialSharedAxis
 import com.zionhuang.innertube.models.SongItem
@@ -20,8 +23,12 @@ import com.zionhuang.music.extensions.toMediaItem
 import com.zionhuang.music.playback.MediaSessionConnection
 import com.zionhuang.music.playback.queues.ListQueue
 import com.zionhuang.music.ui.adapters.YouTubeItemPagingAdapter
+import com.zionhuang.music.ui.adapters.selection.YouTubeItemDetailsLookup
+import com.zionhuang.music.ui.adapters.selection.YouTubeItemKeyProvider
 import com.zionhuang.music.ui.fragments.base.PagingRecyclerViewFragment
+import com.zionhuang.music.ui.listeners.YTItemBatchMenuListener
 import com.zionhuang.music.utils.NavigationEndpointHandler
+import com.zionhuang.music.utils.addActionModeObserver
 import com.zionhuang.music.viewmodels.YouTubeBrowseViewModel
 import com.zionhuang.music.viewmodels.YouTubeBrowseViewModelFactory
 import kotlinx.coroutines.flow.collectLatest
@@ -32,12 +39,39 @@ class YouTubeBrowseFragment : PagingRecyclerViewFragment<YouTubeItemPagingAdapte
     private val viewModel by viewModels<YouTubeBrowseViewModel> { YouTubeBrowseViewModelFactory(requireActivity().application, args.endpoint) }
 
     override val adapter = YouTubeItemPagingAdapter(NavigationEndpointHandler(this))
+    private var tracker: SelectionTracker<String>? = null
+    private val menuListener = YTItemBatchMenuListener(this)
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         enterTransition = MaterialSharedAxis(MaterialSharedAxis.X, true).addTarget(R.id.fragment_content)
         returnTransition = MaterialSharedAxis(MaterialSharedAxis.X, false).addTarget(R.id.fragment_content)
         binding.recyclerView.layoutManager = LinearLayoutManager(requireContext())
+
+        tracker = SelectionTracker.Builder("selectionId", binding.recyclerView, YouTubeItemKeyProvider(adapter), YouTubeItemDetailsLookup(binding.recyclerView), StorageStrategy.createStringStorage())
+            .withSelectionPredicate(SelectionPredicates.createSelectAnything())
+            .build()
+            .apply {
+                adapter.tracker = this
+                addObserver(object : SelectionTracker.SelectionObserver<String>() {
+                    override fun onItemStateChanged(key: String, selected: Boolean) {
+                        getSwipeRefreshLayout().isEnabled = !hasSelection()
+                    }
+                })
+                addActionModeObserver(requireActivity(), R.menu.youtube_item_batch) { menuItem ->
+                    val map = adapter.snapshot().items.associateBy { it.id }
+                    val items = selection.toList().map { map[it] }.filterIsInstance<SongItem>()
+                    when (menuItem.itemId) {
+                        R.id.action_play_next -> menuListener.playNext(items)
+                        R.id.action_add_to_queue -> menuListener.addToQueue(items)
+                        R.id.action_add_to_library -> menuListener.addToLibrary(items)
+                        R.id.action_add_to_playlist -> menuListener.addToPlaylist(items)
+                        R.id.action_download -> menuListener.download(items)
+                    }
+                    true
+                }
+            }
+
         if (args.endpoint.isAlbumEndpoint) {
             adapter.onPlayAlbum = {
                 viewModel.getAlbumSongs()?.let { songs ->
