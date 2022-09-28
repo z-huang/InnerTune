1b7bb572d22fff7555bc043f7cb36e1006b6b84b
Zion Huang <zionhuang1107@gmail.com>
2022-09-28 16:19:31 +0800

    Fix localization code

--- app/src/main/java/com/zionhuang/music/App.kt
+++ app/src/main/java/com/zionhuang/music/App.kt
@@ -18,9 +18,16 @@ class App : Application() {
         INSTANCE = this
 
         val systemDefault = getString(R.string.default_localization_key)
+        val locale = Locale.getDefault()
+        val languageTag = locale.toLanguageTag().replace("-Hant", "") // replace zh-Hant-* to zh-*
+        val languageCodes = resources.getStringArray(R.array.language_codes)
         YouTube.locale = YouTubeLocale(
-            gl = sharedPreferences.getString(getString(R.string.pref_content_country), systemDefault).takeIf { it != systemDefault } ?: Locale.getDefault().country,
-            hl = sharedPreferences.getString(getString(R.string.pref_content_language), systemDefault).takeIf { it != systemDefault } ?: Locale.getDefault().toLanguageTag()
+            gl = sharedPreferences.getString(getString(R.string.pref_content_country), systemDefault).takeIf { it != systemDefault }
+                ?: locale.country,
+            hl = sharedPreferences.getString(getString(R.string.pref_content_language), systemDefault).takeIf { it != systemDefault }
+                ?: if (locale.language in languageCodes) locale.language
+                else if (languageTag in languageCodes) languageTag
+                else "en"
         )
 
         if (sharedPreferences.getBoolean(getString(R.string.pref_proxy_enabled), false)) {
