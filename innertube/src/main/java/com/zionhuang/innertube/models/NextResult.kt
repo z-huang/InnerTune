cc9cb173aa9904bf11532b2026b32dc94d8ef344
Zion Huang <zionhuang1107@gmail.com>
2022-10-12 21:12:31 +0800

    Fix lint

--- innertube/src/main/java/com/zionhuang/innertube/models/NextResult.kt
+++ innertube/src/main/java/com/zionhuang/innertube/models/NextResult.kt
@@ -3,5 +3,6 @@ package com.zionhuang.innertube.models
 data class NextResult(
     val items: List<SongItem>,
     val currentIndex: Int? = null,
+    val lyricsEndpoint: BrowseEndpoint? = null,
     val continuation: String?,
 )
