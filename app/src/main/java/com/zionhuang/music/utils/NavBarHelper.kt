5ee14572acc970231d1998c80a4f08434c40135d
Bnyro <bnyro@tutanota.com>
2022-10-12 19:07:00 +0200

    fix the preference

--- app/src/main/java/com/zionhuang/music/utils/NavBarHelper.kt
+++ app/src/main/java/com/zionhuang/music/utils/NavBarHelper.kt
@@ -1,6 +1,7 @@
 package com.zionhuang.music.utils
 
 import android.content.Context
+import android.util.Log
 import com.zionhuang.music.R
 import com.zionhuang.music.extensions.preference
 import com.zionhuang.music.extensions.sharedPreferences
@@ -11,19 +12,26 @@ class NavBarHelper(
     val pref by context.preference(R.string.visible_tabs, "")
 
     fun getEnabledItems(): BooleanArray {
-        return try {
-            pref.split(",").map {
+        try {
+            val items = pref.split(",").map {
                 it == "true"
             }.toBooleanArray()
+            if (items.size == navBarItemsSize) return items
         } catch (e: Exception) {
-            BooleanArray(5) { true }
+            e.printStackTrace()
         }
+        return BooleanArray(navBarItemsSize) { true }
     }
 
     fun setEnabledItems(enabledItems: BooleanArray) {
+        Log.e("tabs", enabledItems.joinToString(","))
         context.sharedPreferences.edit().putString(
             context.getString(R.string.visible_tabs),
-            enabledItems.joinToString("")
+            enabledItems.joinToString(",")
         ).apply()
+    }
+
+    companion object {
+        const val navBarItemsSize = 5
     }
 }
