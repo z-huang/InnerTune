a253d54048ac676124a02b2267be0db83228fdd5
Zion Huang <zionhuang1107@gmail.com>
2022-10-14 13:08:19 +0800

    Fix #296

--- app/src/main/java/com/zionhuang/music/playback/MusicService.kt
+++ app/src/main/java/com/zionhuang/music/playback/MusicService.kt
@@ -9,16 +9,12 @@ import android.support.v4.media.MediaBrowserCompat
 import android.support.v4.media.MediaBrowserCompat.MediaItem.FLAG_BROWSABLE
 import android.support.v4.media.MediaDescriptionCompat
 import android.support.v4.media.session.MediaSessionCompat
-import android.util.Log
+import androidx.core.content.ContextCompat.startForegroundService
 import androidx.lifecycle.lifecycleScope
 import androidx.media.session.MediaButtonReceiver
 import com.google.android.exoplayer2.ui.PlayerNotificationManager
 
 class MusicService : LifecycleMediaBrowserService() {
-    companion object {
-        private const val TAG = "MusicService"
-    }
-
     private val binder = MusicBinder()
     private lateinit var songPlayer: SongPlayer
 
@@ -31,6 +27,7 @@ class MusicService : LifecycleMediaBrowserService() {
 
             override fun onNotificationPosted(notificationId: Int, notification: Notification, ongoing: Boolean) {
                 if (ongoing) {
+                    startForegroundService(this@MusicService, Intent(this@MusicService, MusicService::class.java))
                     startForeground(notificationId, notification)
                 } else {
                     stopForeground(0)
@@ -42,10 +39,6 @@ class MusicService : LifecycleMediaBrowserService() {
 
     override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
         super.onStartCommand(intent, flags, startId)
-        Log.d(TAG, "onStartCommand: ${intent?.action}")
-        if (intent == null) {
-            return START_STICKY
-        }
         MediaButtonReceiver.handleIntent(songPlayer.mediaSession, intent)
         return START_STICKY
     }
@@ -71,6 +64,7 @@ class MusicService : LifecycleMediaBrowserService() {
             get() = this@MusicService.songPlayer
     }
 
+    // TODO: Support Android Auto
     private val ROOT_ID = "root"
 
     override fun onGetRoot(clientPackageName: String, clientUid: Int, rootHints: Bundle?): BrowserRoot = BrowserRoot(ROOT_ID, null)
