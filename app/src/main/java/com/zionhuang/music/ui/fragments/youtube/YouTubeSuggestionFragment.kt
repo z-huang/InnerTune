d6cb07dcd5b051bdd0145870e11d585aaf4e49ed
Zion Huang <zionhuang1107@gmail.com>
2022-10-26 21:27:12 +0800

    Fix #362

--- app/src/main/java/com/zionhuang/music/ui/fragments/youtube/YouTubeSuggestionFragment.kt
+++ app/src/main/java/com/zionhuang/music/ui/fragments/youtube/YouTubeSuggestionFragment.kt
@@ -1,6 +1,7 @@
 package com.zionhuang.music.ui.fragments.youtube
 
 import android.app.Activity.RESULT_OK
+import android.content.ActivityNotFoundException
 import android.content.Intent
 import android.os.Bundle
 import android.speech.RecognizerIntent
@@ -83,7 +84,10 @@ class YouTubeSuggestionFragment : NavigationFragment<FragmentYoutubeSuggestionBi
             }
         }
         binding.btnVoice.setOnClickListener {
-            voiceResultLauncher.launch(Intent(ACTION_RECOGNIZE_SPEECH))
+            try {
+                voiceResultLauncher.launch(Intent(ACTION_RECOGNIZE_SPEECH))
+            } catch (_: ActivityNotFoundException) {
+            }
         }
         setupSearchView()
         showKeyboard()
