02ab32250627a998ff70f2789db2730249d8b8db
Zion Huang <zionhuang1107@gmail.com>
2022-11-01 15:01:38 +0800

    Temporarily disable regenerate visitorData (#375)

--- app/src/main/java/com/zionhuang/music/viewmodels/SuggestionViewModel.kt
+++ app/src/main/java/com/zionhuang/music/viewmodels/SuggestionViewModel.kt
@@ -1,21 +1,16 @@
 package com.zionhuang.music.viewmodels
 
 import android.app.Application
-import androidx.core.content.edit
 import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.viewModelScope
-import com.zionhuang.innertube.YouTube
 import com.zionhuang.innertube.models.SuggestionTextItem
 import com.zionhuang.innertube.models.SuggestionTextItem.SuggestionSource.LOCAL
 import com.zionhuang.innertube.models.YTBaseItem
-import com.zionhuang.music.R
-import com.zionhuang.music.extensions.sharedPreferences
 import com.zionhuang.music.repos.SongRepository
 import com.zionhuang.music.repos.YouTubeRepository
 import kotlinx.coroutines.launch
 import kotlinx.serialization.ExperimentalSerializationApi
-import kotlinx.serialization.MissingFieldException
 
 class SuggestionViewModel(application: Application) : AndroidViewModel(application) {
     private val songRepository = SongRepository(application)
@@ -36,16 +31,17 @@ class SuggestionViewModel(application: Application) : AndroidViewModel(applicati
                 })
             } catch (e: Exception) {
                 e.printStackTrace()
-                // Remove in future
-                if (e is MissingFieldException) {
-                    // Reset visitorData
-                    YouTube.generateVisitorData().getOrNull()?.let {
-                        getApplication<Application>().sharedPreferences.edit {
-                            putString(getApplication<Application>().getString(R.string.pref_visitor_data), it)
-                        }
-                        YouTube.visitorData = it
-                    }
-                }
+                // Fix incorrect visitorData
+                // comment out because now YouTube Music doesn't give us suggestions if we're not logged in
+//                if (e is MissingFieldException) {
+//                    // Reset visitorData
+//                    YouTube.generateVisitorData().getOrNull()?.let {
+//                        getApplication<Application>().sharedPreferences.edit {
+//                            putString(getApplication<Application>().getString(R.string.pref_visitor_data), it)
+//                        }
+//                        YouTube.visitorData = it
+//                    }
+//                }
             }
         }
     }
