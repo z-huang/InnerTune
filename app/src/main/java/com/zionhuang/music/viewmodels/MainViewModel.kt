44c1e8ca493c3206079c37e1ffa98fd21752f1eb
Zion Huang <zionhuang1107@gmail.com>
2023-01-06 11:22:56 +0800

    Convert list to set to speed up check whether a song is in library

--- app/src/main/java/com/zionhuang/music/viewmodels/MainViewModel.kt
+++ app/src/main/java/com/zionhuang/music/viewmodels/MainViewModel.kt
@@ -5,17 +5,25 @@ import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.viewModelScope
 import com.zionhuang.music.repos.SongRepository
 import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.map
 import kotlinx.coroutines.flow.stateIn
 
 class MainViewModel(application: Application) : AndroidViewModel(application) {
     val songRepository = SongRepository(application)
 
     val librarySongIds = songRepository.getAllSongId()
-        .stateIn(viewModelScope, SharingStarted.Lazily, emptyList())
+        .map(List<String>::toHashSet)
+        .stateIn(viewModelScope, SharingStarted.Lazily, emptySet())
+
     val likedSongIds = songRepository.getAllLikedSongId()
-        .stateIn(viewModelScope, SharingStarted.Lazily, emptyList())
+        .map(List<String>::toHashSet)
+        .stateIn(viewModelScope, SharingStarted.Lazily, emptySet())
+
     val libraryAlbumIds = songRepository.getAllAlbumId()
-        .stateIn(viewModelScope, SharingStarted.Lazily, emptyList())
+        .map(List<String>::toHashSet)
+        .stateIn(viewModelScope, SharingStarted.Lazily, emptySet())
+
     val libraryPlaylistIds = songRepository.getAllPlaylistId()
-        .stateIn(viewModelScope, SharingStarted.Lazily, emptyList())
+        .map(List<String>::toHashSet)
+        .stateIn(viewModelScope, SharingStarted.Lazily, emptySet())
 }
