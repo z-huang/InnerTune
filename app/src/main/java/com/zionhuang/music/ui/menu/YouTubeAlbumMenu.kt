039263c28632633ef98f359b580138ea79b85d14
Zion Huang <zionhuang1107@gmail.com>
2023-06-27 00:02:57 +0800

    Complete YouTube album menu

--- app/src/main/java/com/zionhuang/music/ui/menu/YouTubeAlbumMenu.kt
+++ app/src/main/java/com/zionhuang/music/ui/menu/YouTubeAlbumMenu.kt
@@ -13,9 +13,11 @@ import androidx.compose.foundation.layout.systemBars
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
@@ -25,20 +27,29 @@ import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.core.net.toUri
+import androidx.media3.exoplayer.offline.Download
+import androidx.media3.exoplayer.offline.DownloadRequest
+import androidx.media3.exoplayer.offline.DownloadService
 import androidx.navigation.NavController
 import com.zionhuang.innertube.YouTube
 import com.zionhuang.innertube.models.AlbumItem
+import com.zionhuang.innertube.pages.AlbumPage
 import com.zionhuang.music.LocalDatabase
+import com.zionhuang.music.LocalDownloadUtil
 import com.zionhuang.music.R
 import com.zionhuang.music.constants.ListItemHeight
+import com.zionhuang.music.db.entities.PlaylistSongMap
+import com.zionhuang.music.extensions.toMediaItem
+import com.zionhuang.music.models.toMediaMetadata
+import com.zionhuang.music.playback.ExoDownloadService
 import com.zionhuang.music.playback.PlayerConnection
 import com.zionhuang.music.playback.queues.YouTubeAlbumRadio
+import com.zionhuang.music.ui.component.DownloadGridMenu
 import com.zionhuang.music.ui.component.GridMenu
 import com.zionhuang.music.ui.component.GridMenuItem
 import com.zionhuang.music.ui.component.ListDialog
 import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.launch
 
 @Composable
 fun YouTubeAlbumMenu(
@@ -50,7 +61,66 @@ fun YouTubeAlbumMenu(
 ) {
     val context = LocalContext.current
     val database = LocalDatabase.current
+    val downloadUtil = LocalDownloadUtil.current
     val libraryAlbum by database.album(album.id).collectAsState(initial = null)
+    var albumPage: AlbumPage? by remember {
+        mutableStateOf(null)
+    }
+
+    LaunchedEffect(Unit) {
+        YouTube.album(album.browseId).onSuccess {
+            albumPage = it
+        }
+    }
+
+    var downloadState by remember {
+        mutableStateOf(Download.STATE_STOPPED)
+    }
+
+    LaunchedEffect(albumPage) {
+        val songs = albumPage?.songs?.map { it.id } ?: return@LaunchedEffect
+        downloadUtil.downloads.collect { downloads ->
+            downloadState =
+                if (songs.all { downloads[it]?.state == Download.STATE_COMPLETED })
+                    Download.STATE_COMPLETED
+                else if (songs.all {
+                        downloads[it]?.state == Download.STATE_QUEUED
+                                || downloads[it]?.state == Download.STATE_DOWNLOADING
+                                || downloads[it]?.state == Download.STATE_COMPLETED
+                    })
+                    Download.STATE_DOWNLOADING
+                else
+                    Download.STATE_STOPPED
+        }
+    }
+
+    var showChoosePlaylistDialog by rememberSaveable {
+        mutableStateOf(false)
+    }
+
+    AddToPlaylistDialog(
+        isVisible = showChoosePlaylistDialog,
+        onAdd = { playlist ->
+            var position = playlist.songCount
+            database.transaction {
+                albumPage?.let { albumPage ->
+                    albumPage.songs
+                        .map { it.toMediaMetadata() }
+                        .onEach(::insert)
+                        .forEach { song ->
+                            insert(
+                                PlaylistSongMap(
+                                    songId = song.id,
+                                    playlistId = playlist.id,
+                                    position = position++
+                                )
+                            )
+                        }
+                }
+            }
+        },
+        onDismiss = { showChoosePlaylistDialog = false }
+    )
 
     var showSelectArtistDialog by rememberSaveable {
         mutableStateOf(false)
@@ -117,16 +187,20 @@ fun YouTubeAlbumMenu(
         }
         GridMenuItem(
             icon = R.drawable.playlist_play,
-            title = R.string.play_next,
-            enabled = false
+            title = R.string.play_next
         ) {
+            albumPage?.songs
+                ?.map { it.toMediaItem() }
+                ?.let(playerConnection::playNext)
             onDismiss()
         }
         GridMenuItem(
             icon = R.drawable.queue_music,
-            title = R.string.add_to_queue,
-            enabled = false
+            title = R.string.add_to_queue
         ) {
+            albumPage?.songs
+                ?.map { it.toMediaItem() }
+                ?.let(playerConnection::addToQueue)
             onDismiss()
         }
         if (libraryAlbum != null) {
@@ -143,30 +217,45 @@ fun YouTubeAlbumMenu(
                 icon = R.drawable.library_add,
                 title = R.string.add_to_library
             ) {
-                coroutineScope.launch(Dispatchers.IO) {
-                    YouTube.album(album.browseId).onSuccess { albumPage ->
-                        database.query {
-                            insert(albumPage)
-                        }
-                    }
+                database.transaction {
+                    albumPage?.let(::insert)
                 }
             }
         }
 
         GridMenuItem(
             icon = R.drawable.playlist_add,
-            title = R.string.add_to_playlist,
-            enabled = false
+            title = R.string.add_to_playlist
         ) {
-
+            showChoosePlaylistDialog = true
         }
-        GridMenuItem(
-            icon = R.drawable.download,
-            title = R.string.download,
-            enabled = false
-        ) {
-
-        }
+        DownloadGridMenu(
+            state = downloadState,
+            onDownload = {
+                albumPage?.songs?.forEach { song ->
+                    val downloadRequest = DownloadRequest.Builder(song.id, song.id.toUri())
+                        .setCustomCacheKey(song.id)
+                        .setData(song.title.toByteArray())
+                        .build()
+                    DownloadService.sendAddDownload(
+                        context,
+                        ExoDownloadService::class.java,
+                        downloadRequest,
+                        false
+                    )
+                }
+            },
+            onRemoveDownload = {
+                albumPage?.songs?.forEach { song ->
+                    DownloadService.sendRemoveDownload(
+                        context,
+                        ExoDownloadService::class.java,
+                        song.id,
+                        false
+                    )
+                }
+            }
+        )
         album.artists?.let { artists ->
             GridMenuItem(
                 icon = R.drawable.artist,
