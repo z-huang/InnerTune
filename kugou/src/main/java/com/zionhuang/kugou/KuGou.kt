c7edab5b90cc586dfde6b2d057ca7717bd794722
Zion Huang <zionhuang1107@gmail.com>
2023-06-27 12:40:28 +0800

    Fix some songs do not reflect KuGou lyrics

--- kugou/src/main/java/com/zionhuang/kugou/KuGou.kt
+++ kugou/src/main/java/com/zionhuang/kugou/KuGou.kt
@@ -28,7 +28,7 @@ object KuGou {
     var useTraditionalChinese: Boolean = false
 
     @OptIn(ExperimentalSerializationApi::class)
-    private val client = HttpClient(OkHttp) {
+    private val client = HttpClient {
         expectSuccess = true
 
         install(ContentNegotiation) {
@@ -45,10 +45,6 @@ object KuGou {
         install(ContentEncoding) {
             gzip()
             deflate()
-        }
-
-        defaultRequest {
-            url("https://krcs.kugou.com")
         }
     }
 
@@ -93,35 +89,41 @@ object KuGou {
         return searchLyricsByKeyword(keyword, duration).candidates.firstOrNull()
     }
 
-    private suspend fun searchSongs(keyword: Pair<String, String>) = client.get("https://mobileservice.kugou.com/api/v3/search/song") {
-        parameter("version", 9108)
-        parameter("plat", 0)
-        parameter("keyword", "${keyword.first} - ${keyword.second}")
-    }.body<SearchSongResponse>()
+    private suspend fun searchSongs(keyword: Pair<String, String>) =
+        client.get("https://mobileservice.kugou.com/api/v3/search/song") {
+            parameter("version", 9108)
+            parameter("plat", 0)
+            parameter("pagesize", 8)
+            parameter("showtype", 0)
+            url.encodedParameters.append("keyword", "${keyword.first} - ${keyword.second}".encodeURLParameter(spaceToPlus = false))
+        }.body<SearchSongResponse>()
 
-    private suspend fun searchLyricsByKeyword(keyword: Pair<String, String>, duration: Int) = client.get("https://lyrics.kugou.com/search") {
-        parameter("ver", 1)
-        parameter("man", "yes")
-        parameter("client", "pc")
-        parameter("keyword", "${keyword.first} - ${keyword.second}")
-        parameter("duration", duration.takeIf { it != -1 }?.times(1000)) // if duration == -1, we don't care duration
-    }.body<SearchLyricsResponse>()
+    private suspend fun searchLyricsByKeyword(keyword: Pair<String, String>, duration: Int) =
+        client.get("https://lyrics.kugou.com/search") {
+            parameter("ver", 1)
+            parameter("man", "yes")
+            parameter("client", "pc")
+            parameter("duration", duration.takeIf { it != -1 }?.times(1000)) // if duration == -1, we don't care duration
+            url.encodedParameters.append("keyword", "${keyword.first} - ${keyword.second}".encodeURLParameter(spaceToPlus = false))
+        }.body<SearchLyricsResponse>()
 
-    private suspend fun searchLyricsByHash(hash: String) = client.get("https://lyrics.kugou.com/search") {
-        parameter("ver", 1)
-        parameter("man", "yes")
-        parameter("client", "pc")
-        parameter("hash", hash)
-    }.body<SearchLyricsResponse>()
+    private suspend fun searchLyricsByHash(hash: String) =
+        client.get("https://lyrics.kugou.com/search") {
+            parameter("ver", 1)
+            parameter("man", "yes")
+            parameter("client", "pc")
+            parameter("hash", hash)
+        }.body<SearchLyricsResponse>()
 
-    private suspend fun downloadLyrics(id: Long, accessKey: String) = client.get("https://lyrics.kugou.com/download") {
-        parameter("fmt", "lrc")
-        parameter("charset", "utf8")
-        parameter("client", "pc")
-        parameter("ver", 1)
-        parameter("id", id)
-        parameter("accesskey", accessKey)
-    }.body<DownloadLyricsResponse>()
+    private suspend fun downloadLyrics(id: Long, accessKey: String) =
+        client.get("https://lyrics.kugou.com/download") {
+            parameter("fmt", "lrc")
+            parameter("charset", "utf8")
+            parameter("client", "pc")
+            parameter("ver", 1)
+            parameter("id", id)
+            parameter("accesskey", accessKey)
+        }.body<DownloadLyricsResponse>()
 
     private fun normalizeTitle(title: String) = title
         .replace("\\(.*\\)".toRegex(), "")
@@ -143,39 +145,40 @@ object KuGou {
 
     fun generateKeyword(title: String, artist: String) = normalizeTitle(title) to normalizeArtist(artist)
 
-    private fun String.normalize(keyword: Pair<String, String>): String = replace("&apos;", "'").lines().filter { line ->
-        line matches ACCEPTED_REGEX
-    }.let {
-        // Remove useless information such as singer, writer, composer, guitar, etc.
-        var headCutLine = 0
-        for (i in min(30, it.lastIndex) downTo 0) {
-            if (it[i] matches BANNED_REGEX) {
-                headCutLine = i + 1
-                break
+    private fun String.normalize(keyword: Pair<String, String>): String =
+        replace("&apos;", "'").lines().filter { line ->
+            line matches ACCEPTED_REGEX
+        }.let {
+            // Remove useless information such as singer, writer, composer, guitar, etc.
+            var headCutLine = 0
+            for (i in min(30, it.lastIndex) downTo 0) {
+                if (it[i] matches BANNED_REGEX) {
+                    headCutLine = i + 1
+                    break
+                }
             }
-        }
-        it.drop(headCutLine)
-    }.let {
-        var tailCutLine = 0
-        for (i in min(it.size - 30, it.lastIndex) downTo 0) {
-            if (it[it.lastIndex - i] matches BANNED_REGEX) {
-                tailCutLine = i + 1
-                break
+            it.drop(headCutLine)
+        }.let {
+            var tailCutLine = 0
+            for (i in min(it.size - 30, it.lastIndex) downTo 0) {
+                if (it[it.lastIndex - i] matches BANNED_REGEX) {
+                    tailCutLine = i + 1
+                    break
+                }
             }
+            it.dropLast(tailCutLine)
+        }.let { lines ->
+            val firstLine = lines.firstOrNull()?.toSimplifiedChinese() ?: return@let lines
+            val (title, artist) = keyword
+            if (title.toSimplifiedChinese() in firstLine ||
+                artist.split("、").any { it.toSimplifiedChinese() in firstLine }
+            ) {
+                lines.drop(1)
+            } else lines
+        }.joinToString(separator = "\n").let {
+            if (useTraditionalChinese) it.normalizeForTraditionalChinese()
+            else it
         }
-        it.dropLast(tailCutLine)
-    }.let { lines ->
-        val firstLine = lines.firstOrNull()?.toSimplifiedChinese() ?: return@let lines
-        val (title, artist) = keyword
-        if (title.toSimplifiedChinese() in firstLine ||
-            artist.split("、").any { it.toSimplifiedChinese() in firstLine }
-        ) {
-            lines.drop(1)
-        } else lines
-    }.joinToString(separator = "\n").let {
-        if (useTraditionalChinese) it.normalizeForTraditionalChinese()
-        else it
-    }
 
     private fun String.normalizeForTraditionalChinese() =
         if (none { c -> UnicodeScript.of(c.code) in JapaneseUnicodeScript }) toTraditionalChinese()
