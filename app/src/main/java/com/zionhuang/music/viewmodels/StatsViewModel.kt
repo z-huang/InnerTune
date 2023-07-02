c099b9c8ff55f956447f7da2789bd51ff54c56ff
Zion Huang <zionhuang1107@gmail.com>
2023-07-02 10:17:49 +0800

    Fix #731

--- app/src/main/java/com/zionhuang/music/viewmodels/StatsViewModel.kt
+++ app/src/main/java/com/zionhuang/music/viewmodels/StatsViewModel.kt
@@ -2,10 +2,14 @@ package com.zionhuang.music.viewmodels
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.zionhuang.innertube.YouTube
 import com.zionhuang.music.db.MusicDatabase
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.SharingStarted
 import kotlinx.coroutines.flow.stateIn
+import kotlinx.coroutines.launch
+import java.time.Duration
+import java.time.LocalDateTime
 import javax.inject.Inject
 
 @HiltViewModel
@@ -16,4 +20,23 @@ class StatsViewModel @Inject constructor(
         .stateIn(viewModelScope, SharingStarted.Lazily, emptyList())
     val mostPlayedArtists = database.mostPlayedArtists()
         .stateIn(viewModelScope, SharingStarted.Lazily, emptyList())
+
+    init {
+        viewModelScope.launch {
+            mostPlayedArtists.collect { artists ->
+                artists
+                    .map { it.artist }
+                    .filter {
+                        it.thumbnailUrl == null || Duration.between(it.lastUpdateTime, LocalDateTime.now()) > Duration.ofDays(10)
+                    }
+                    .forEach { artist ->
+                        YouTube.artist(artist.id).onSuccess { artistPage ->
+                            database.query {
+                                update(artist, artistPage)
+                            }
+                        }
+                    }
+            }
+        }
+    }
 }
