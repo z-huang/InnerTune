038aab277f6d3986f939f93f1e1121eb86748fc7
Zion Huang <zionhuang1107@gmail.com>
2023-08-09 22:26:01 +0800

    Tweak code

--- app/src/main/java/com/zionhuang/music/ui/menu/PlayerMenu.kt
+++ app/src/main/java/com/zionhuang/music/ui/menu/PlayerMenu.kt
@@ -8,9 +8,9 @@ import androidx.annotation.DrawableRes
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.foundation.layout.asPaddingValues
 import androidx.compose.foundation.layout.fillMaxWidth
@@ -18,7 +18,7 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.systemBars
-import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.Icon
@@ -26,7 +26,6 @@ import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
-import androidx.compose.material3.OutlinedButton
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
@@ -34,10 +33,8 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
-import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.platform.LocalConfiguration
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
@@ -54,6 +51,7 @@ import androidx.media3.exoplayer.offline.DownloadService
 import androidx.navigation.NavController
 import com.zionhuang.music.LocalDatabase
 import com.zionhuang.music.LocalDownloadUtil
+import com.zionhuang.music.LocalPlayerConnection
 import com.zionhuang.music.R
 import com.zionhuang.music.constants.ListItemHeight
 import com.zionhuang.music.db.entities.PlaylistSongMap
@@ -69,7 +67,6 @@ import com.zionhuang.music.ui.component.ListDialog
 import kotlin.math.log2
 import kotlin.math.pow
 import kotlin.math.round
-import kotlin.math.roundToInt
 
 @Composable
 fun PlayerMenu(
@@ -83,7 +80,6 @@ fun PlayerMenu(
     mediaMetadata ?: return
     val context = LocalContext.current
     val database = LocalDatabase.current
-    val localConfiguration = LocalConfiguration.current
     val playerVolume = playerConnection.service.playerVolume.collectAsState()
     val activityResultLauncher = rememberLauncherForActivityResult(ActivityResultContracts.StartActivityForResult()) { }
 
@@ -146,31 +142,12 @@ fun PlayerMenu(
         }
     }
 
-    var tempo by remember {
-        mutableStateOf(playerConnection.player.playbackParameters.speed)
-    }
-    var transposeValue by remember {
-        mutableStateOf(round(12 * log2(playerConnection.player.playbackParameters.pitch)).toInt())
-    }
-    val updatePlaybackParameters = {
-        playerConnection.player.playbackParameters = PlaybackParameters(tempo, 2f.pow(transposeValue.toFloat() / 12))
-    }
-    var showPitchTempoDialog by remember {
+    var showPitchTempoDialog by rememberSaveable {
         mutableStateOf(false)
     }
 
     if (showPitchTempoDialog) {
         PitchTempoDialog(
-            tempo = tempo,
-            transposeValue = transposeValue,
-            onTempoChange = { newTempo ->
-                tempo = newTempo
-                updatePlaybackParameters()
-            },
-            onTransposeChange = { newTranspose ->
-                transposeValue = newTranspose
-                updatePlaybackParameters()
-            },
             onDismiss = { showPitchTempoDialog = false }
         )
     }
@@ -308,13 +285,77 @@ fun PlayerMenu(
 }
 
 @Composable
+fun PitchTempoDialog(
+    onDismiss: () -> Unit,
+) {
+    val playerConnection = LocalPlayerConnection.current ?: return
+    var tempo by remember {
+        mutableStateOf(playerConnection.player.playbackParameters.speed)
+    }
+    var transposeValue by remember {
+        mutableStateOf(round(12 * log2(playerConnection.player.playbackParameters.pitch)).toInt())
+    }
+    val updatePlaybackParameters = {
+        playerConnection.player.playbackParameters = PlaybackParameters(tempo, 2f.pow(transposeValue.toFloat() / 12))
+    }
+
+    AlertDialog(
+        properties = DialogProperties(usePlatformDefaultWidth = false),
+        onDismissRequest = onDismiss,
+        dismissButton = {
+            TextButton(
+                onClick = {
+                    tempo = 1f
+                    transposeValue = 0
+                    updatePlaybackParameters()
+                }
+            ) {
+                Text(stringResource(R.string.reset))
+            }
+        },
+        confirmButton = {
+            TextButton(
+                onClick = onDismiss
+            ) {
+                Text(stringResource(android.R.string.ok))
+            }
+        },
+        text = {
+            Column {
+                ValueAdjuster(
+                    icon = R.drawable.slow_motion_video,
+                    currentValue = tempo,
+                    values = listOf(0.25f, 0.5f, 0.75f, 1f, 1.25f, 1.5f, 1.75f, 2f),
+                    onValueUpdate = {
+                        tempo = it
+                        updatePlaybackParameters()
+                    },
+                    valueText = { "x$it" },
+                    modifier = Modifier.padding(bottom = 12.dp)
+                )
+                ValueAdjuster(
+                    icon = R.drawable.discover_tune,
+                    currentValue = transposeValue,
+                    values = (-12..12).toList(),
+                    onValueUpdate = {
+                        transposeValue = it
+                        updatePlaybackParameters()
+                    },
+                    valueText = { "${if (it > 0) "+" else ""}$it" }
+                )
+            }
+        }
+    )
+}
+
+@Composable
 fun <T> ValueAdjuster(
     @DrawableRes icon: Int,
     currentValue: T,
     values: List<T>,
     onValueUpdate: (T) -> Unit,
     valueText: (T) -> String,
-    modifier: Modifier,
+    modifier: Modifier = Modifier,
 ) {
     Row(
         horizontalArrangement = Arrangement.spacedBy(24.dp),
@@ -343,7 +384,7 @@ fun <T> ValueAdjuster(
             text = valueText(currentValue),
             style = MaterialTheme.typography.titleMedium,
             textAlign = TextAlign.Center,
-            modifier = Modifier.weight(1f)
+            modifier = Modifier.width(80.dp)
         )
 
         IconButton(
@@ -358,106 +399,4 @@ fun <T> ValueAdjuster(
             )
         }
     }
-}
-
-@Composable
-fun PitchTempoDialog(
-    tempo: Float,
-    transposeValue: Int,
-    onTempoChange: (Float) -> Unit,
-    onTransposeChange: (Int) -> Unit,
-    onDismiss: () -> Unit
-) {
-    var initialTempo by remember { mutableStateOf(tempo) }
-    var initialTransposeValue by remember { mutableStateOf(transposeValue) }
-    var tempoState by remember { mutableStateOf(tempo) }
-    var transposeState by remember { mutableStateOf(transposeValue) }
-    var resetClicked by remember { mutableStateOf(false) }
-
-    LaunchedEffect(Unit) {
-        initialTempo = tempo
-        initialTransposeValue = transposeValue
-    }
-
-    AlertDialog(
-        properties = DialogProperties(usePlatformDefaultWidth = false),
-        onDismissRequest = {
-            if (!resetClicked) {
-                onTempoChange(initialTempo)
-                onTransposeChange(initialTransposeValue)
-            }
-            onDismiss()
-        },
-        icon = { Icon(painter = painterResource(R.drawable.tune), contentDescription = null) },
-        title = { Text(stringResource(R.string.playback_control)) },
-        confirmButton = {
-            TextButton(onClick = {
-                onDismiss()
-            }) {
-                Text(stringResource(android.R.string.ok))
-            }
-        },
-        dismissButton = {
-            TextButton(onClick = {
-                if (!resetClicked) {
-                    onTempoChange(initialTempo)
-                    onTransposeChange(initialTransposeValue)
-                }
-                onDismiss()
-            }) {
-                Text(stringResource(android.R.string.cancel))
-            }
-        },
-        text = {
-            LazyColumn() {
-                item {
-                    ValueAdjuster(
-                        icon = R.drawable.slow_motion_video,
-                        currentValue = tempoState,
-                        values = listOf(0.25f, 0.5f, 0.75f, 1f, 1.25f, 1.5f, 1.75f, 2f),
-                        onValueUpdate = { newTempo ->
-                            tempoState = newTempo
-                            onTempoChange(newTempo)
-                        },
-                        valueText = { "x$it" },
-                        modifier = Modifier
-                            .fillMaxWidth()
-                            .padding(horizontal = 24.dp, vertical = 6.dp)
-                    )
-                }
-                item {
-                    ValueAdjuster(
-                        icon = R.drawable.discover_tune,
-                        currentValue = transposeState.toFloat(),
-                        values = (-12..12).toList().map { it.toFloat() },
-                        onValueUpdate = { newTranspose ->
-                            transposeState = newTranspose.roundToInt()
-                            onTransposeChange(newTranspose.roundToInt())
-                        },
-                        valueText = { "${if (it > 0) "+" else ""}${it.roundToInt()}" },
-                        modifier = Modifier
-                            .fillMaxWidth()
-                            .padding(horizontal = 24.dp, vertical = 6.dp)
-                    )
-                }
-                item {
-                    Spacer(modifier=Modifier.height(8.dp))
-                    Row(
-                        horizontalArrangement=Arrangement.Center,
-                        modifier=Modifier.fillMaxWidth()
-                    ) {
-                        OutlinedButton(onClick={
-                            resetClicked=true
-                            tempoState=1.0f
-                            transposeState=0
-                            onTempoChange(1.0f)
-                            onTransposeChange(0)
-                        }) {
-                            Text(stringResource(R.string.reset))
-                        }
-                    }
-                }
-            }
-        }
-    )
 }
