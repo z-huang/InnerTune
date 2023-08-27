767cd72d66856aae9501de7fa0e64cfd2eaf5be0
Zion Huang <zionhuang1107@gmail.com>
2023-08-27 13:39:23 +0800

    Fix notification bitmap error

--- app/src/main/java/com/zionhuang/music/utils/CoilBitmapLoader.kt
+++ app/src/main/java/com/zionhuang/music/utils/CoilBitmapLoader.kt
@@ -7,11 +7,13 @@ import android.graphics.drawable.BitmapDrawable
 import android.net.Uri
 import androidx.media3.session.BitmapLoader
 import coil.imageLoader
+import coil.request.ErrorResult
 import coil.request.ImageRequest
 import com.google.common.util.concurrent.ListenableFuture
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.guava.future
+import java.util.concurrent.ExecutionException
 
 class CoilBitmapLoader(
     private val context: Context,
@@ -30,6 +32,13 @@ class CoilBitmapLoader(
                     .allowHardware(false)
                     .build()
             )
-            (result.drawable as BitmapDrawable).bitmap
+            if (result is ErrorResult) {
+                throw ExecutionException(result.throwable)
+            }
+            try {
+                (result.drawable as BitmapDrawable).bitmap
+            } catch (e: Exception) {
+                throw ExecutionException(e)
+            }
         }
 }
