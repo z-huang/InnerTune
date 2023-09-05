b382fb3327759d541a7dbda115de07baf8df419c
Zion Huang <zionhuang1107@gmail.com>
2023-09-05 09:55:58 +0800

    Show library artist albums first in new releases

--- app/src/main/java/com/zionhuang/music/viewmodels/NewReleaseViewModel.kt
+++ app/src/main/java/com/zionhuang/music/viewmodels/NewReleaseViewModel.kt
@@ -4,22 +4,41 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.zionhuang.innertube.YouTube
 import com.zionhuang.innertube.models.AlbumItem
+import com.zionhuang.music.db.MusicDatabase
+import com.zionhuang.music.db.entities.Artist
 import com.zionhuang.music.utils.reportException
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.first
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
-class NewReleaseViewModel @Inject constructor() : ViewModel() {
+class NewReleaseViewModel @Inject constructor(
+    database: MusicDatabase,
+) : ViewModel() {
     private val _newReleaseAlbums = MutableStateFlow<List<AlbumItem>>(emptyList())
     val newReleaseAlbums = _newReleaseAlbums.asStateFlow()
 
     init {
         viewModelScope.launch {
-            YouTube.newReleaseAlbums().onSuccess {
-                _newReleaseAlbums.value = it
+            YouTube.newReleaseAlbums().onSuccess { albums ->
+                val artists: Set<String>
+                val favouriteArtists: Set<String>
+                database.artistsByCreateDateAsc().first().let { list ->
+                    artists = list.map(Artist::id).toHashSet()
+                    favouriteArtists = list
+                        .filter { it.artist.bookmarkedAt != null }
+                        .map { it.id }
+                        .toHashSet()
+                }
+                _newReleaseAlbums.value = albums
+                    .sortedBy { album ->
+                        if (album.artists.orEmpty().any { it.id in favouriteArtists }) 0
+                        else if (album.artists.orEmpty().any { it.id in artists }) 1
+                        else 2
+                    }
             }.onFailure {
                 reportException(it)
             }
